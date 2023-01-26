2480e0fd83e34f8092180fdd4263cf7da79270e1
yusriyusoff <muhammad.yusri@graduate.utm.my>
2023-01-27 00:52:54 +0800

    coding schedule tk siap lagi

--- src/main/java/com/homs/demo/controller/scheduleController.java
+++ src/main/java/com/homs/demo/controller/scheduleController.java
@@ -9,11 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.homs.demo.dbutil.StaffDAO;
+import com.homs.demo.model.Staff;
+
 @Controller
 public class scheduleController {
 
     @GetMapping(value="/setSchedule")
     public String setSchedule() {
+        Staff staff = null;
+        StaffDAO staffDAO = new StaffDAO();
 
         return "adminSetSchedule";
     }
