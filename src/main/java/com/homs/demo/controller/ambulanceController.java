05507dd9362055edbd08fa4ec79ed38e52b8f260
yusriyusoff <muhammad.yusri@graduate.utm.my>
2023-01-26 10:17:34 +0800

    not yet finish ambulance

---
+++ src/main/java/com/homs/demo/controller/ambulanceController.java
@@ -0,0 +1,22 @@
+package com.homs.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class ambulanceController {
+    
+    @GetMapping(value="/choosePlate")
+    public String choosePlate() {
+        return "ambulance";
+    }
+
+    @PostMapping(value="/checkAmbulance")
+    public String check(@RequestParam("select_ambulance")String plateNum){
+        return "ambulanceRespond";
+    }
+    
+}
