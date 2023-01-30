8ae4ec7c15ee4918f4d367ade0abc2a1d968861a
megatirfanzack <megatirfanzack@graduate.utm.my>
2023-01-31 02:28:07 +0800

    Last

--- src/main/java/com/homs/demo/controller/queueController.java
+++ src/main/java/com/homs/demo/controller/queueController.java
@@ -24,41 +24,51 @@ public class queueController {
 
     @GetMapping("queueController")
     public String queue(HttpSession session) {
+        //check if the patient is logged in
         Patient patient = (Patient) session.getAttribute("patient");
         if (patient == null) {
-            return "redirect:/login";
+            return "redirected:/login";
         }
         return "queueForm";
     }
     @RequestMapping("/EnterQueue")
     public String enterQueue(HttpSession session, Model model, HttpServletRequest request) {
-        
-        // Get the patient attribute from the session
+        //initialize the queueDAO and patient object
         QueueDAO queueDAO = new QueueDAO();
         Patient patient = null;
+        
+        // Get the patient attribute from the session
         patient = (Patient) session.getAttribute("patient");
+        //check if the patient is logged in
         if (patient == null) {
             return "redirect:/login";
         } 
         else{
+            //check if the patient is already in the queue
             Queue queue = queueDAO.getQueueByPatient(patient);
+            //if not, add the patient to the queue
             if (queue == null) {
+                //create a new queue object and add it to the database
                 queue = new Queue(patient.getPatientID(), "Waiting...", Timestamp.valueOf(LocalDateTime.now()));
                 int row = queueDAO.enterVirtualQueue(queue);
                 model.addAttribute("queue", queue);
 
             }
+            // if the patient is already in the queue, return the queue
             else{
                 model.addAttribute("queue", queue);
 
             }
 
         }
+        //get the medical history from the form
         String medicalHistory = request.getParameter("medicalHistory");
         Record record = new Record(patient.getPatientID(), medicalHistory);
+        //add the medical history to the database
         RecordDAO recordDAO = new RecordDAO();
         int row = recordDAO.enterRecord(record);
-
+        
+        //get all the queue objects into a lists from the database
         java.util.List<Map<String, Object>> queues = queueDAO.getAllQueue();
         model.addAttribute("queues", queues);
         return "queue";        
